ecc38675f75f88d7607d0b84600720666ecf72e6
Aki Sasaki <aki@mozilla.com>
2020-12-08 10:12:03 -0800

    add escapewindow/android-components to relpro projects

--- taskcluster/ac_taskgraph/release_promotion.py
+++ taskcluster/ac_taskgraph/release_promotion.py
@@ -17,6 +17,7 @@ from taskgraph.util.taskgraph import find_decision_task, find_existing_tasks_fro
 
 RELEASE_PROMOTION_PROJECTS = (
     "https://github.com/mozilla-mobile/android-components",
+    "https://github.com/escapewindow/android-components",
 )
 
 
